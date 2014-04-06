a6eec4ed5f477de3279730393535bd2599903882
Asad Amjad <asad_dbest@yahoo.com>
2014-04-07 01:54:41 +0500

    method

--- gpacalc.rb
+++ gpacalc.rb
@@ -1,6 +1,36 @@
 #By Asad amjad
 #Program used to calculate GPA
 
-puts "Enter number of subjects = #{no=gets.chomp.to_i}"
+puts "=============================================================================="
+puts "||                      GPA Calculator - By ASAD AMJAD                      ||"
+puts "=============================================================================="
+puts "                                                                              "
+
+def convert_grade_to_num (letter_grade)
+	grade = 0;
+	case letter_grade
+	when "A"
+		grade = 4.0
+	when "B+"
+		grade = 3.5
+	when "B"
+		grade = 3
+	when "C+"
+		grade = 2.5
+	when "C"
+		grade = 2
+	when "D+"
+		grade = 1.5
+	when "D"
+		grade = 1
+	when "F"
+		grade = 0
+	end
+	grade
+end
+puts "Enter your grade"
+user_input=gets.chomp
+puts "your grade #{user_input} = #{convert_grade_to_num (user_input)} "
+
 
 
